6b5980328cbc13a8067888664c3472d4aae1fd3e
Jessica <jessicaimoesemi@gmail.com>
2025-09-14 23:13:20 +0100

    await dispatch before success message

--- src/features/accounts/Deposit.jsx
+++ src/features/accounts/Deposit.jsx
@@ -28,7 +28,7 @@ function Deposit() {
         setSuccess(`You've deposited $${depositAmt} successfully.`);
         setDepositAmt("");
       } else {
-        dispatch(depositAsync(depositAmt, currency));
+        await dispatch(depositAsync(depositAmt, currency));
         setSuccess(`You've deposited ${depositAmt} ${currency} successfully.`);
         setDepositAmt("");
       }
